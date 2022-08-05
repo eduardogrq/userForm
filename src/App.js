5a153059c7df6e92f4eace2e51e2a627bef5980d
Eduardogrq <eduardo.grquinonez@gmail.com>
2022-08-04 23:38:51 -0500

    Creating a function to detect changes in an input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [formulario, setFormulario] = useState({name: ''})
+  const handleChange = (e) => {
+    setFormulario({
+      ...formulario,
+      [e.target.name]: e.target.value
+    })
+  }
+  console.log(formulario)
+  return(
+    <form>
+      <input name="name" type="text" onChange={handleChange}/>
+    </form>
+  )
 }
 
-export default App;
+export default App
